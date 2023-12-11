9cf9113bf0f426cea2d6b64ca7380499b1484ca1
jamal <ouardijamal87@gmail.com>
2023-12-11 10:24:24 +0100

    first

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "M205_TP_1"
+include(":app")
